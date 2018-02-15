69b530ccb23f838d85770aa601622486421d21df
root <root@verissimo-Inspiron-5457>
2018-02-14 22:18:28 -0200

    Commit inicial

---
+++ src/main/java/br/com/joaoverissimo/curso/pontointeligente/ApiRestfulSpringBootApplication.java
@@ -0,0 +1,12 @@
+package br.com.joaoverissimo.curso.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootApplication.class, args);
+	}
+}
