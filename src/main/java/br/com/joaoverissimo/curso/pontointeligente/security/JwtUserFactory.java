fe1ae5cd725ee329bea663d9cc6d52587ba806fe
verissimo <verissimo@verissimo-Inspiron-5457>
2018-02-28 20:58:52 -0300

    Auth JWT

---
+++ src/main/java/br/com/joaoverissimo/curso/pontointeligente/security/JwtUserFactory.java
@@ -0,0 +1,28 @@
+package br.com.joaoverissimo.curso.pontointeligente.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import br.com.joaoverissimo.curso.pontointeligente.entities.Funcionario;
+import br.com.joaoverissimo.curso.pontointeligente.enums.PerfilEnum;
+
+public class JwtUserFactory {
+
+    private JwtUserFactory() {
+    }
+
+    public static JwtUser create(Funcionario funcionario) {
+        return new JwtUser(funcionario.getId(), funcionario.getEmail(), funcionario.getSenha(),
+                mapToGrantedAuthorities(funcionario.getPerfil()));
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(PerfilEnum perfilEnum) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority(perfilEnum.toString()));
+        return authorities;
+    }
+
+}
---
+++ src/main/java/br/com/joaoverissimo/curso/pontointeligente/security/service/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package br.com.joaoverissimo.curso.pontointeligente.security.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.joaoverissimo.curso.pontointeligente.entities.Funcionario;
+import br.com.joaoverissimo.curso.pontointeligente.security.JwtUserFactory;
+import br.com.joaoverissimo.curso.pontointeligente.service.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+        if (funcionario.isPresent()) {
+            return JwtUserFactory.create(funcionario.get());
+        }
+
+        throw new UsernameNotFoundException("Email não encontrado.");
+    }
+
+}
